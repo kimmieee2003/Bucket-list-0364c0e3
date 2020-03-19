141f13c185baadb6688400ef43e5eeb974f180d7
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-19 12:14:23 +0100

    Uitwerking van de exercise

---
+++ bucket_list.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hoe veel activiteiten wil je toevoegen?";
+$activiteiten = readline();
+if(is_numeric($activiteiten)) {
+    $bucket = array("");
+for($i=1; $i <= $activiteiten; $i++){
+  echo "welke activiteiten wil je toevoegen?";
+  $item = readline();
+  array_push($bucket, $item);
+}
+echo "In uw bucketlist staat:" ."\n";
+for($c = 0; $c <= $activiteiten; $c++){
+echo "$bucket[$c]"."\n";
+}
+}else{
+    echo "dit is geen getal.";
+}
+?>
